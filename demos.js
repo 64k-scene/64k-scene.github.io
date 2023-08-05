25fa58b835758abb774f4d941f8ac4b03f1c3374
Laurent Le Brun <laurentlb@gmail.com>
2023-08-05 23:49:19 +0200

    Move style and demo list in separate files
    + minor simplification in html

---
+++ demos.js
@@ -0,0 +1,66 @@
+const allDemos = [
+{
+    "pouet": "65342",
+    "title": "Offscreen Colonies",
+    "group": "Conspiracy",
+    "youtube": "PaNYOzwlVC8",
+    "date": "2015-04-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00065/00065342.jpg",
+    "demozoo": "136150"
+},
+{
+    "pouet": "59106",
+    "title": "F - Felix's Workshop",
+    "group": "Ctrl-Alt-Test",
+    "youtube": "6CiF034IhgY",
+    "date": "2012-04-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00059/00059106.jpg",
+    "demozoo": "31428"
+},
+
+{
+    "pouet": "62935",
+    "title": "the timeless",
+    "group": "Mercury",
+    "youtube": "ie4u2i_5OdE",
+    "date": "2014-04-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00062/00062935.jpg",
+    "demozoo": "109116"
+},
+{
+    "pouet": "67113",
+    "title": "fermi paradox",
+    "group": "Mercury",
+    "youtube": "gX7ESVZ7T6Y",
+    "date": "2016-03-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00067/00067113.jpg",
+    "demozoo": "155057"
+},
+{
+    "pouet": "67106",
+    "title": "Darkness Lay Your Eyes Upon Me",
+    "group": "Conspiracy",
+    "youtube": "qRVTI2y0BBg",
+    "date": "2016-03-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00067/00067106.jpg",
+    "demozoo": "155058"
+},
+{
+    "pouet": "61204",
+    "title": "Turtles all the way down",
+    "group": "Brain Control",
+    "youtube": "sBKmqkh9bb8",
+    "date": "2013-03-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00061/00061204.jpg",
+    "demozoo": "59943"
+},
+{
+    "pouet": "88524",
+    "title": "Clean Slate",
+    "group": "Conspiracy",
+    "youtube": "O3T1-nadehU",
+    "date": "2021-04-15",
+    "screenshot": "https://content.pouet.net/files/screenshots/00088/00088524.jpg",
+    "demozoo": "292282"
+}
+];
